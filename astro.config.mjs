4d6ee22456dcd6a6bcb6e274112d157722113291
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2025-11-04 00:38:23 -0300

    remove stylex . Global CSS CONFIG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import stylex from 'astro-stylex';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), stylex()]
+	integrations: [react()]
 });
