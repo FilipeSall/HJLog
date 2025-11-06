5a85381bb6277a7c46b9cb65cf77713873ede879
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2025-11-06 19:28:13 -0300

    config hero-section

---
+++ src/data/hero-section.ts
@@ -0,0 +1,13 @@
+import type { StepsItemProps } from "../types/components";
+
+export const stepsFlowsData: StepsItemProps[] = [
+  {
+    label: "Planejamento da rota"
+  },
+  {
+    label: "Carga e monitoramento"
+  },
+  {
+    label: "Carga e monitoramento"
+  }
+]
--- src/types/components.ts
+++ src/types/components.ts
@@ -1,12 +1,17 @@
 export type NavLinkLabel =
   | string
   | {
-      src: string;
-      alt: string;
-    };
+    src: string;
+    alt: string;
+  };
 
 export interface NavLinkProps {
   label: NavLinkLabel;
   path: string;
   target?: string;
+}
+
+export interface StepsItemProps {
+  label: string;
+  i?: number;
 }
