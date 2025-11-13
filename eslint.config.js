6166afb1e37181b3c1a3cffa0e33119abf2ca3aa
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2025-11-12 23:11:21 -0300

    feat: quem somos section

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import eslintPluginImport from "eslint-plugin-import";
+import astro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist", "node_modules", ".astro"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs["flat/recommended"],
+  {
+    plugins: {
+      import: eslintPluginImport,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
+  },
+];
