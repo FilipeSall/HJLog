f6865014cefa6212c29cdafc08c05db5c302d170
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2025-11-25 20:55:53 -0300

    feat: servicos sectionn

--- src/data/servicos.ts
+++ src/data/servicos.ts
@@ -1,4 +1,11 @@
+import type { ImageMetadata } from "astro";
 import type { ServiceItemProps } from "../types/components";
+import frame1 from "../images/servicos/frame-1.png";
+import frame2 from "../images/servicos/frame-2.png";
+import frame3 from "../images/servicos/frame-3.png";
+import frame4 from "../images/servicos/frame-4.png";
+import frame5 from "../images/servicos/frame-5.png";
+
 
 export const servicosData: ServiceItemProps[] = [
   {
@@ -9,4 +16,12 @@ export const servicosData: ServiceItemProps[] = [
     title: "Logística corporativa",
     text: "Transferência entre Centros de Distribuição e abastecimento de lojas. Atuação com grandes redes."
   }
+];
+
+export const servicosImages: ImageMetadata[] = [
+  frame1,
+  frame2,
+  frame3,
+  frame4,
+  frame5
 ];
