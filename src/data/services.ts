3f0ad64a6ca5bb895f5deb5828d7c9a6e2c5978d
Filipe Reges de Salles <filipe.salles@seatecnologia.com.br>
2025-12-15 01:58:22 -0300

    novas imagens serviocs

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,5 +1,7 @@
 import corporateLogisticsImage from "../images/transportes.webp";
+import dedicatedLoadImage from "../images/carga.webp";
 import showsEventsImage from "../images/evento.webp";
+import warehouseImage from "../images/armazem.webp";
 
 export const services = {
   heading: {
@@ -23,9 +25,8 @@ export const services = {
       description:
         "Instalações seguras e climatizadas para otimizar sua distribuição.",
       icon: "warehouse",
-      image:
-        "https://lh3.googleusercontent.com/aida-public/AB6AXuDal1Qrps1bWK4ZTKk7wEvsOYChTSBWCi1H8TqNgVlzMHQaphsn7VXIKKmEDnD4mfwF2GRgZWpOKQ568Jj7KH462AKRIeuzZte7JzXb08g04mMNDd0ZDXb2gHwQcimZ0tD89TlZCrm3S1Y-Nq1agHJ3c1GPQi9uAZmS-M6exqtMLQcfpTnKMXtuDCK2fd85JvaVE1BZooW3MIFgahLJwwCBRaiZuG9jTy4i9D58qBQ60PupZbeHoWkWvr30IqZRHqkCDXV9ej4E-2bt",
-      imageAlt: "Armazém logístico",
+      image: warehouseImage.src,
+      imageAlt: "Estrutura de armazenagem",
       featured: false,
     },
     {
@@ -42,9 +43,8 @@ export const services = {
       description:
         "Veículos exclusivos para sua carga com monitoramento dedicado.",
       icon: "inventory_2",
-      image:
-        "https://lh3.googleusercontent.com/aida-public/AB6AXuCPhWshxKHmWeqAXwhZrN2-3d8aJ1IEA47WMjIiArgwj4XZWCLSoiLxi8tGjI7e05SnEb1J6EpJQVmSzk6RBY8wNtkCVS1xAtAaU9yK6WwZlt5q7t-yhqttjxnosIgtmdrPFQ7SBsZituSTP8lZBygqcuWvkAs4OEhDHP_dm4Q3pBGDkDbpY3HTi5hHOqT-dR2aQmb7uQmXbwGkZY58CVa8LoJNRiBd5_qidFnRAHPXmrySZ-gHXLcgD4taHJ5S-gfvZiLMyxNPOIlO",
-      imageAlt: "Carga segura",
+      image: dedicatedLoadImage.src,
+      imageAlt: "Carga dedicada em transporte especializado",
       featured: false,
     },
   ],
